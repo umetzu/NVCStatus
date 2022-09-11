fc93090781a37b809b2b54ebcf0f93ebadb01fd2
Dennis Umetzu <dennis@umetzu.com>
2022-09-11 15:42:33 -0400

    inline comparison

--- NVCResults/Data/NVCForecastService.cs
+++ NVCResults/Data/NVCForecastService.cs
@@ -1,3 +1,4 @@
+using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
 
 namespace NVCResults.Data
@@ -15,7 +16,117 @@ namespace NVCResults.Data
             return result;
         }
 
-        public Task<List<Root>> GetForecastAsync(string dateLog)
+        public SortedDictionary<string, (int, int, int)> GetSummary(List<Root> log)
+        {
+            var result = new SortedDictionary<string, (int, int, int)>();
+
+            var previous = log.GroupBy(x => x.nvcCaseInfo?.previousStatus).Select(x => new { status = x?.Key, count = x?.Count() }).OrderBy(x => x.status);
+            var current = log.GroupBy(x => x.nvcCaseInfo?.status).Select(x => new { status = x?.Key, count = x?.Count() }).OrderBy(x => x.status);
+            var next = log.GroupBy(x => x.nvcCaseInfo?.nextStatus).Select(x => new { status = x?.Key, count = x?.Count() }).OrderBy(x => x.status);
+
+            foreach (var item in previous)
+            {
+                if (!string.IsNullOrEmpty(item.status))
+                {
+                    result.TryGetValue(item.status ?? "", out (int, int, int) value);
+                    value.Item1 = item.count ?? 0;
+                    result[item.status ?? ""] = value;
+                }
+            }
+
+            foreach (var item in current)
+            {
+                if (!string.IsNullOrEmpty(item.status))
+                {
+                    result.TryGetValue(item.status ?? "", out (int, int, int) value);
+                    value.Item2 = item.count ?? 0;
+                    result[item.status ?? ""] = value;
+                }
+            }
+
+            foreach (var item in next)
+            {
+                if (!string.IsNullOrEmpty(item.status))
+                {
+                    result.TryGetValue(item.status ?? "", out (int, int, int) value);
+                    value.Item3 = item.count ?? 0;
+                    result[item.status ?? ""] = value;
+                }
+            }
+
+            result["Total"] = (log.Count(x => !string.IsNullOrEmpty(x.nvcCaseInfo.previousStatus)),
+                log.Count(x => !string.IsNullOrEmpty(x.nvcCaseInfo.status)),
+                log.Count(x => !string.IsNullOrEmpty(x.nvcCaseInfo.nextStatus)));
+
+            return result;
+        }
+
+        public Task<List<Root>> GetForecastAsync(string dateLog, List<string> logs)
+        {
+            int dateLogIndex = logs.IndexOf(dateLog);
+
+            List<Root> currentLog = ReadLog(dateLog);
+
+            string nextDateLog = "", previousDateLog = "";
+
+            if (dateLogIndex < logs.Count - 1)
+            {
+                nextDateLog = logs[dateLogIndex + 1];
+                var nextLog = ReadLog(nextDateLog);
+
+                List<Root> toAdd = new List<Root>();
+
+                foreach (var next in nextLog)
+                {
+                    Root? current = currentLog.FirstOrDefault(x => x.nvcCaseInfo.caseNumber == next.nvcCaseInfo.caseNumber);
+
+                    if (current == null)
+                    {
+                        current = next;
+                        current.nvcCaseInfo.nextStatus = current.nvcCaseInfo.status;
+                        current.nvcCaseInfo.status = "";
+                        toAdd.Add(current);
+                    }
+                    else
+                    {
+                        current.nvcCaseInfo.nextStatus = next.nvcCaseInfo.status;
+                    }
+                }
+
+                currentLog.AddRange(toAdd);
+            }
+
+            if (dateLogIndex  - 1 >= 0)
+            {
+                previousDateLog = logs[dateLogIndex - 1];
+                var previousLog = ReadLog(previousDateLog);
+
+                List<Root> toAdd = new List<Root>();
+
+                foreach (var previous in previousLog)
+                {
+                    Root? current = currentLog.FirstOrDefault(x => x.nvcCaseInfo.caseNumber == previous.nvcCaseInfo.caseNumber);
+
+                    if (current == null)
+                    {
+                        current = previous;
+                        current.nvcCaseInfo.previousStatus = current.nvcCaseInfo.status;
+                        current.nvcCaseInfo.status = "";
+                        toAdd.Add(current);
+                    }
+                    else
+                    {
+                        current.nvcCaseInfo.previousStatus = previous.nvcCaseInfo.status;
+                    }
+                }
+
+                currentLog.AddRange(toAdd);
+            }
+
+            return Task.FromResult(currentLog.OrderBy(x => x.nvcCaseInfo.caseNumber).ToList());
+        }
+
+        private List<Root> ReadLog(string dateLog)
         {
             var results = new List<Root>();
 
@@ -31,14 +142,14 @@ namespace NVCResults.Data
 
                     if (logItem != null && logItem.nvcCaseInfo != null)
                     {
-                       results.Add(logItem);
+                        results.Add(logItem);
                     }
                 }
             }
 
             results = results.OrderBy(x => x.nvcCaseInfo.caseNumber).ToList();
 
-            return Task.FromResult(results);
+            return results;
         }
     }
 }
--- NVCResults/Data/Root.cs
+++ NVCResults/Data/Root.cs
@@ -4,11 +4,59 @@ namespace NVCResults.Data
     {
         public string caseNumber { get; set; }
         public string status { get; set; }
+        public string nextStatus { get; set; }
+        public string previousStatus { get; set; }
         public string createdDate { get; set; }
         public string lastUpdatedDate { get; set; }
         public string location { get; set; }
         public string applicationType { get; set; }
         public string message { get; set; }
+
+        public string previousStatusClass
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(previousStatus))
+                {
+                    return previousStatus != status ? "table-danger text-danger" : "";
+                }
+
+                return "";
+            }
+        }
+        public string nextStatusClass
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(nextStatus))
+                {
+                    return nextStatus != status ? "table-danger text-danger" : "";
+                }
+
+                return "";
+            }
+        }
+
+        public bool isDistinct
+        {
+            get
+            {
+                bool result = false;
+
+                if (!string.IsNullOrEmpty(previousStatus))
+                {
+                    result = previousStatus != status;
+                }
+
+                if (!result || !string.IsNullOrEmpty(nextStatus))
+                {
+                    result = nextStatus != status;
+                }
+
+                return result;
+            }
+        }
+
         public string estimatedDate =>
              new DateTime(2020, 1, 1).AddDays(int.Parse(caseNumber.Substring(7, 3)) - 501).ToString("dd-MMM-yyyy");
 
